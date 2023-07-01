d98e4bc27aa1147313af9ae76a7c91c718fffde4
basanta <basanta.gurung180@gmail.com>
2023-07-01 13:41:21 +0545

    crud app added

--- login/urls.py
+++ login/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from fun import urls
+from crud import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('fun.urls'))
+    path("", include('fun.urls')),
+    path("crud/", include("crud.urls"))
 ]
